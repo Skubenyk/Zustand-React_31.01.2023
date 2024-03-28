9eb614ea5a54e58bf185b95cebbd3b42368b89b5
Yevhenii Skubenyk <skubenik@gmail.com>
2024-03-28 14:26:33 +0200

    add project structure, devtools, persist

--- src/components/users/userStore.ts
+++ src/components/users/userStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
-// import { devtools } from 'zustand/middleware/devtools';
+import { devtools, persist } from 'zustand/middleware';
 
 export interface User {
   id: number;
@@ -16,33 +16,40 @@ export interface UsersState {
 }
 
 export const useUsersStore = create<UsersState>()(
-  immer((set) => ({
-    users: [],
-    isLoading: false,
-    errors: [],
-    // addUser: (username: string) =>
-    //   set((state) => ({ users: [...state.users, { id: Date.now(), username }] })),
+  persist(
+    devtools(
+      immer((set) => ({
+        users: [],
+        isLoading: false,
+        errors: [],
+        // addUser: (username: string) =>
+        //   set((state) => ({ users: [...state.users, { id: Date.now(), username }] })),
 
-    addUser: (username: string) =>
-      set((state) => {
-        state.users.push({ id: Date.now(), username });
-      }),
+        addUser: (username: string) =>
+          set((state) => {
+            state.users.push({ id: Date.now(), username });
+          }),
 
-    fetchUsers: async () => {
-      const result = await fetch('https://jsonplaceholder.typicode.com/users');
-      const json = (await result.json()) as User[];
-      set({ users: json });
-    },
+        fetchUsers: async () => {
+          const result = await fetch(
+            'https://jsonplaceholder.typicode.com/users'
+          );
+          const json = (await result.json()) as User[];
+          set({ users: json });
+        },
 
-    //*With Axios
-    // fetchUsers: async () => {
-    //   set({ isLoading: true, errors: [] });
-    //   try {
-    //     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-    //     const users = response.data as User[];
-    //     set({ users, isLoading: false });
-    //   } catch (error) {
-    //     set({ isLoading: false, errors: ['Error fetching users'] });
-    //   }
-  }))
+        //*With Axios
+        // fetchUsers: async () => {
+        //   set({ isLoading: true, errors: [] });
+        //   try {
+        //     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        //     const users = response.data as User[];
+        //     set({ users, isLoading: false });
+        //   } catch (error) {
+        //     set({ isLoading: false, errors: ['Error fetching users'] });
+        //   }
+      }))
+    ),
+    { name: 'usersStore', version: 1 }
+  )
 );
